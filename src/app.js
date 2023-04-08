c74da07fbdc256df328b63ab65b57c6343aea3b9
SolForte <mateusbrandao1234@gmail.com>
2023-04-08 00:11:01 -0300

    Formatting

--- src/app.js
+++ src/app.js
@@ -11,27 +11,24 @@ const TWEETS = [];
 
 app.post("/sign-up", (req, res) => {
   const { username, avatar } = req.body;
-
   REGISTERED_USERS.push({ username, avatar });
-
-  res.status(200).send("OK");
+  return res.status(200).send("OK");
 });
 
 app.post("/tweets", (req, res) => {
   const { username, tweet } = req.body;
-
   if (!REGISTERED_USERS.find((e) => e.username === username)) {
     return res.status(401).send("UNAUTHORIZED");
   }
-
   TWEETS.push({ username, tweet });
-
-  res.status(201).send("OK");
+  return res.status(201).send("OK");
 });
 
 app.get("/tweets", (req, res) => {
-  res.status(200).send("OK");
+  return res.status(200).send("OK");
 });
 
 const PORT = 5000;
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
