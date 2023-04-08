29409b12973d4979dd251ebcc442370b5c2d8ce1
SolForte <mateusbrandao1234@gmail.com>
2023-04-07 23:09:54 -0300

    ES module, requisições vazias

--- src/app.js
+++ src/app.js
@@ -2,8 +2,24 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
+
 app.use(cors());
-app.use(json());
+app.use(express.json());
+
+const USERS = [];
+const TWEETS = [];
+
+app.post("/sign-up", (req, res) => {
+    res.status(200).send("OK");
+})
+
+app.post("/tweets", (req, res) => {
+    res.status(200).send("OK");
+})
+
+app.get("/tweets", (req, res) => {
+    res.status(200).send("OK");
+})
 
 const PORT = 5000;
 app.listen(PORT);
